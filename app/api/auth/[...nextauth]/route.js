db117c7ffea24238225a5b2f98b9173864316f94
Vedanty5501 <Vedanty5501@gmail.com>
2024-06-12 17:02:58 +0530

    signIn callback

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { signIn } from 'next-auth/react';
 
 const authOptions = {
   session: {
@@ -13,6 +14,10 @@ const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks:{
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log(user,profile)
+      return true
+    },
     async callback(url, req) {
         return `${req.protocol}://${req.get('host')}/api/auth/callback/${url}`;
       },
